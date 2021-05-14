c2a4e157c24235e5b43ad575f94e889cc798f285
Makhuta <matykubla@gmail.com>
2021-05-14 23:25:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
     array_move(hodnoty) {return require("./files/array_move")(hodnoty)},
     check_user_for_language(hodnoty) {return require("./files/check_user_for_language")(hodnoty)},
+    function_load(hodnoty) {return require("./files/check_user_for_language")(hodnoty.bot, hodnoty.main_dir)},
 }
