8c492f9b11f1cbbd538d9f746465612cb87d4c85
Makhuta <matykubla@gmail.com>
2021-05-14 23:36:07 +0200

    update

---
+++ files/language_load.js
@@ -0,0 +1,21 @@
+const fs = require("fs")
+
+module.exports = {
+    init(main_dir) {
+        let languages = main_dir + "/lang/"
+        fs.readdir(languages, (err, langs) => {
+            langs.forEach(lang => {
+                let per_cmd_map = new Map();
+                fs.readdir(languages + lang + "/", (err, lang_files) => {
+                    lang_files.forEach(file => {
+                        let file_name = file.split(".")[0].toUpperCase()
+                        let file_content = require("." + languages + lang + "/" + file)
+                        per_cmd_map.set(file_name, file_content)
+                    })
+                    module.exports.languages.set(lang, per_cmd_map)
+                })
+            })
+        })
+    },
+    languages: new Map()
+}
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
-    array_move(hodnoty) {return require("./files/array_move")(hodnoty)},
-    check_user_for_language(hodnoty) {return require("./files/check_user_for_language")(hodnoty)},
-    function_load(hodnoty) {return require("./files/functions_load")(hodnoty.bot, hodnoty.main_dir)},
+    array_move(hodnoty) { return require("./files/array_move")(hodnoty) },
+    check_user_for_language(hodnoty) { return require("./files/check_user_for_language")(hodnoty) },
+    function_load(hodnoty) { return require("./files/functions_load")(hodnoty.bot, hodnoty.main_dir) },
+    language_load(hodnoty) { return require("./files/language_load")(hodnoty.main_dir) },
 }
